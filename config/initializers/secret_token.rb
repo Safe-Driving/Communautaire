e9531e06455866157ae693ad1290897b37047895
Matthieu <68857@supinfo.com>
2011-03-14 15:28:18 +0100

    Initial commit in supinfo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Communautaire::Application.config.secret_token = '24fc0c8cffd0d47031413425ce5a97e9602a93db49f31256114f9af177c7bf093bc5120dd2c872275f8bfb4b791527d5f112326830775ff333c9de07e3efbe3f'
