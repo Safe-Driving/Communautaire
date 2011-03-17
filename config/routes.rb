0f59ee98ee7fd38a8309fb34ec77314dd72b1271
Romain <68727@supinfo.com>
2011-03-17 21:17:36 +0100

    Nouvelle Page Home et debut d'integration du design

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Communautaire::Application.routes.draw do
-  root :to => "pages/home"
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
