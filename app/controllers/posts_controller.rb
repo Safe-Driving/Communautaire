63675ed0082260050399931ab0c7d824e0069a06
Florian PETIT <florian.p37@gmail.com>
2011-03-28 21:39:24 +0200

    added a controller (forum posts)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,12 @@
-class PostsController
-  # To change this template use File | Settings | File Templates.
+=begin
+  Controller pour la gestion des posts
+
+  @author : Florian Petit
+=end
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.order('created_at DESC').last(3)
+  end
+
 end
