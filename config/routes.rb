ba2105d882df4d92a97f146145f7332fb541230b
Florian PETIT <florian.p37@gmail.com>
2011-03-29 14:45:43 +0200

    added a controller (forum posts)

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,5 @@ Communautaire::Application.routes.draw do
   match '/forum', :to => 'topics#index'
   root :to => 'pages#home'
 
-<<<<<<< HEAD
 
-
-=======
->>>>>>> b09a877b0e0b412328028e2d0a9d9a7623b91193
 end
