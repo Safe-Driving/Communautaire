4cc7ac58fbc32fe3140b788a16338b3b5aa7703d
Florian PETIT <florian.p37@gmail.com>
2011-03-29 22:49:25 +0200

    merged routes.rb
    Signed-off-by: Florian PETIT <florian.p37@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,21 @@
 Communautaire::Application.routes.draw do
 
-<<<<<<< HEAD
 
 
 
-  get "posts/new"
 
-  get "posts/index"
-
-  get "posts/show"
-
-  get "posts/edit"
-=======
   get "sessions/new"
 
   get "users/new"
->>>>>>> 4643a885308a235758b9e18ca82fc5d4b7bc1ee7
 
   get "users/create"
 
   resources :users
   resources :articles
   resources :topics
-<<<<<<< HEAD
   resources :categories
   resources :posts
-=======
   resources :sessions, :only => [:new, :create, :destroy]
->>>>>>> 4643a885308a235758b9e18ca82fc5d4b7bc1ee7
 
   match '/home',    :to => 'pages#home'
   match '/forum', :to => 'categories#index'
