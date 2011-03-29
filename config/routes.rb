46a085274b1c77e011df95a67fdc16d36bea6872
Romain <68727@supinfo.com>
2011-03-29 14:57:04 +0200

    Conflit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Communautaire::Application.routes.draw do
 
+
+  resources :users
   resources :articles
   resources :topics
 
+  match '/signup',  :to => 'users#new'
   match '/home',    :to => 'pages#home'
   match '/forum', :to => 'topics#index'
+
   root :to => 'pages#home'
+
+  get "users/new"
 
 
 end
