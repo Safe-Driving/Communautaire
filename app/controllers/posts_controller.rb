c1c616d0ec780ce92790fdba9e9c0507f82e8c7f
Florian PETIT <florian.p37@gmail.com>
2011-03-30 09:32:16 +0200

    Ok pour la navigation dans le forum
    Forum > categories > topics > posts
    
    voir comment faire pour avoir une belle url du type
    
    domain.tld/forum/idcategorie-nomCategorie/idtopic-nomTopic/idpost-nomPost
    
    et pourquoi pas ajouter un fil d'ariane.
    
    Signed-off-by: Florian PETIT <florian.p37@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,12 @@ class PostsController < ApplicationController
   def new
   end
 
-  def index
+def index
+    @posts = Post.order('created_at DESC').last(3)
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
