44e4132bee31a3c2c94f070864da420a9b303988
Romain <68727@supinfo.com>
2011-04-04 16:43:46 +0200

    Password security updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,14 +42,14 @@ class User < ActiveRecord::Base
 
 
     def has_password?(submitted_password)
-      encrypted_password == encrypt(submitted_password)
+      password == encrypt(submitted_password)
     end
 
     private
 
     def encrypt_password
       self.salt = make_salt if new_record?
-      self.encrypted_password = encrypt(password)
+      self.password = encrypt(password)
     end
 
     def encrypt(string)
