0ce12e2c0112d4ea3a6f00cd687d6919a37098ca
Romain <68727@supinfo.com>
2011-04-04 17:11:02 +0200

    Validation et affichage des erreurs du formulaire de sign in MOUAHAHAH

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to "http://google.fr"
+      redirect_to @user
     else
-      @user.inspect
-      Time.now.inspect
+      render 'new'
     end
   end
 
