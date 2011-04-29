d66c453171561671c68aea1c6684fd80bdd4ca15
Anthony <68471@supinfo.com>
2011-04-30 01:33:16 +0200

    modif articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,15 +21,31 @@ class ArticlesController < ApplicationController
   @category = Category.find(params[:category][:id])
   @article.category = @category
   @article.author = current_user.id
-  @article.save
+   if @article.save
+     flash[:notice] = 'Article was successfully created.'
+    redirect_to :root
+   else
+     flash[:error] = 'Article was not created.'
+     redirect_to :new
   end
-  
+  end
+
   def index
     @articles = Article.order('created_at DESC').limit(3)
+    if !current_user.nil?
+    if current_user.administrator?
+      @addarticle = "New Article"
+    end
+    end
   end
   
   def show
     @article = Article.find(params[:id])
+     if !current_user.nil?
+    if current_user.administrator?
+      @editarticle = "Edit"
+    end
+     end
   end
 
   def update
@@ -41,16 +57,21 @@ class ArticlesController < ApplicationController
   @article.content = @content
   @title =   params[:article][:title]
   @article.title =  @title
+
+
   if @article.save
     redirect_to :root
+   flash[:notice] = 'Article was successfully updated.'
   else
-    redirect_to :articles
+    flash[:error] = 'Article was not updated.'
+    redirect_to :edit
   end
   end
 
   def edit
   
   @article = Article.find(params[:id])
+
   end
 
 end
